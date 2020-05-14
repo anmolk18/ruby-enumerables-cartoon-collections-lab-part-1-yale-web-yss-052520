21dc10c55929216b8f341cbcafa72ca8e449d461
Anmolpreet Kaur <anmolpreet.kaur@yale.edu>
2020-05-14 21:02:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def greet_characters(array)
   #
   # Print a custom greeting for each element
   array.each do |greeting|
-    puts "Hello #{array.capitalize}!"
+    puts "Hello #{array}!"
   end 
 end
 
